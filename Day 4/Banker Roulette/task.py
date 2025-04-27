eae18bee664d1768745b33419888cc7a268779fb
Nelson <nelsonokiki@gmail.com>
2025-04-27 14:50:09 +0100

    adding to the 100 days of coding tutorial

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,11 @@
-friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+import random
+
+friends = []
+number_of_people = int(input('How many people do you want to input: '))
+for i in range(number_of_people):
+    friend_name = input('Enter name: ')
+    friends.append(friend_name)
+print(random.choice(friends))
+print(friends)
+
+# "Alice", "Bob", "Charlie", "David", "Emanuel"
--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -2,4 +2,8 @@ import random
 
 heads = 'heads'
 tails = 'tails'
-print(random.[heads, tails])
+my_random = random.randint(0,1)
+if my_random == 1:
+    print(heads)
+elif my_random == 0:
+    print(tails)
