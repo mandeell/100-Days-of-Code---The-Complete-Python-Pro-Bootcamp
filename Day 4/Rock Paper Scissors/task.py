cd8da61b9196ebe462afdd3ce363409494b3e1b8
Nelson <nelsonokiki@gmail.com>
2025-04-29 15:18:29 +0100

    added to day 4 task

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -27,35 +27,25 @@ scissors = '''
 ---.__(___)
 '''
 
-name_of_games = ['Rock', 'Paper' 'Scissors']
 game = [rock, paper, scissors]
 # event = [name_of_games, game]
 my_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: '))
-computer_game = random.choice(name_of_games[0])
-computer_choice = random.choice(game[1])
-user_choice = ''
-if my_choice == 0:
-    user_choice = name_of_games[0]
-elif my_choice == 1:
-    user_choice = name_of_games[1]
-elif my_choice == 2:
-    user_choice = name_of_games[2]
+if my_choice >= 0 and my_choice <= 2:
+    print(f'\nYou chose: \n{game[my_choice]}')
 
+computer_choice = random.randint(0,2)
+print('Computer chose: ')
+print(game[computer_choice])
 
-if my_choice == 0 and computer_choice == game[2]:
-    print(f'\nYou chose: {game[0]}')
-    print(f'\n Computer chose: {computer_choice}')
-    print('You Win!')
-elif my_choice == 2 and computer_choice == game[1]:
-    print(f'\nYou chose: {game[2]}')
-    print(f'\n Computer chose: {computer_choice}')
-    print('You Win!')
-elif my_choice == 1 and computer_choice == game[0]:
-    print(f'\nYou chose: {game[1]}')
-    print(f'\n Computer chose: {computer_choice}')
-    print('You Win!')
-else:
-    print(f'\nYou chose: {user_choice}')
-    print(f'\n Computer chose: {computer_choice}')
-    print('You Lose!')
-
+if my_choice >= 3 or my_choice < 0:
+    print('You typed an invalid number. You Lose!')
+elif my_choice == 0 and computer_choice == 2:
+    print('You win!')
+elif computer_choice == 0 and my_choice == 2:
+    print('You lose!')
+elif computer_choice > my_choice:
+    print('You lose!')
+elif my_choice > computer_choice:
+    print('You win!')
+elif computer_choice == my_choice:
+    print("It's a draw!")
