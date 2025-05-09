b954788613c5d5b2f498a3971bb08c7f38d909c3
Nelson <nelsonokiki@gmail.com>
2025-05-09 17:14:08 +0100

    added to day 10 and 11 tasks

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -1,7 +1,17 @@
-def format_name(f_name, l_name):
-    formated_f_name = f_name.title()
-    formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+# def format_name(f_name, l_name):
+#     formated_f_name = f_name.title()
+#     formated_l_name = l_name.title()
+#     return f"{formated_f_name} {formated_l_name}"
+#
+#
+# print(format_name("AnGEla", "YU"))
 
 
-print(format_name("AnGEla", "YU"))
+def is_leap_year(year):
+    # Write your code here.
+    # Don't change the function name.
+    if year % 4 and year % 400:
+        return True
+    else:
+        return False
+print(is_leap_year(2000))
