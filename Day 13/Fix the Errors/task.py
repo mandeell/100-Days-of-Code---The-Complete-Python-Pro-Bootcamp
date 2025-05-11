124c41ba05a9cf8b94684ffd77f2eb25c12f5342
Nelson <nelsonokiki@gmail.com>
2025-05-11 23:05:04 +0100

    added to day 13 and 14 tasks

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,11 @@
-age = int(input("How old are you?"))
-if age > 18:
-print("You can drive at age {age}.")
+while True:
+    try:
+        age = int(input("How old are you? "))
+        if age > 18:
+            print(f"You can drive at age {age}.")
+            break
+        else:
+            print("Please enter a valid integer age.")
+    except ValueError:
+        print("You can't mix integers with strings in a comparison")
+
